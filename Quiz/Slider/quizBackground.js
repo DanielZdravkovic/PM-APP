e75b4fda761cb3bccccf185378c4b879f76e7faf
Daweenci <we3bk1ng@gmail.com>
2025-03-22 13:53:35 +0100

    buttons fuer slider Quiz fertig gestellt

--- Quiz/Slider/quizBackground.js
+++ Quiz/Slider/quizBackground.js
@@ -1,35 +1,29 @@
-            const slider = document.getElementById("customSlider");
-            const output = document.getElementById("sliderValue");
-            const checkAnswerButton = document.getElementById("checkAnswer");
-            const answer = document.getElementById("answer");
-            const backButton = document.getElementById("backButton");
+    function transformValue(val) { //Zahlen anpassen automatisch ueber json implementieren
+        console.log(val);
+        if (val < 10) {
+            return  10 - val; 
+        } 
+        else {
+            return val - 8; 
+        }
+    }
         
-            function transformValue(val) { //Zahlen anpassen automatisch ueber json implementieren
-                console.log(val);
-                if (val < 10) {
-                    return  10 - val; 
-                } 
-                else {
-                    return val - 8; 
-                }
-            }
-        
-            slider.addEventListener("input", function () {
-                output.textContent = transformValue(Number(slider.value));
-            });
+    document.getElementById("customSlider").addEventListener("input", function () {
+        document.getElementById("sliderValue").textContent = transformValue(Number(document.getElementById("customSlider").value));
+    });
 
-            function checkAnswer() {
-                const answerValue = document.getElementById("customSlider").value; 
-                alert(answerValue);
-                answer.style.visibility = "visible";
-                nextButton.style.visibility = "visible";
-                checkAnswerButton.style.visibility = "hidden";
-            }
+    function checkAnswer() {
+        const answerValue = document.getElementById("customSlider").value; 
+        alert(answerValue);
+        document.getElementById("answer").style.visibility = "visible";
+        nextButton.style.visibility = "visible";
+        document.getElementById("checkAnswer").style.visibility = "hidden";
+    }
 
-            function back() {
-                window.location.href = "../index.html";
-            }
+    function back() {
+        window.location.href = "../index.html";
+    }
             
-            checkAnswerButton.addEventListener("click", checkAnswer);
+    document.getElementById("checkAnswer").addEventListener("click", checkAnswer);
 
-            backButton.addEventListener("click", back);
+    document.getElementById("backButton").addEventListener("click", back);
--- Quiz/quizBackground.js
+++ Quiz/quizBackground.js
@@ -1,14 +1,34 @@
     document.addEventListener("DOMContentLoaded", function () {
+
+        let jsonObjects = [];
+
+        async function fetchJsonObjects() {
+            try {
+                const response = await fetch("./quizJson.json");
+                const quizDaten = await response.json();
+                console.log(quizDaten);
+                return quizDaten;
+            } catch (error) {
+                console.error("Error loading quiz data:", error);
+                return [];
+            }
+        }
+
+        fetchJsonObjects().then(data => {
+            jsonObjects = data;
+            runQuizScript();
+        });
+                
         function runQuizScript() {
 
-        function getRandomQuizObject(){
-            fetchJsonObjects().then(quizDaten => {
-                const zufaelligerIndex = Math.floor(Math.random() * quizDaten.length);
-                const quizObjekt = quizDaten[zufaelligerIndex];
+            function getRandomQuizObject(){        
+                const zufaelligerIndex = Math.floor(Math.random() * jsonObjects.length);
+
+                const quizObject = jsonObjects[0];//eigentlich zufaelligerIndex
                 const containerId = "quizContainer";
-                console.log(quizObjekt);
-                document.getElementById("quizContainer").innerHTML = quizObjekt.frage;
-                switch (quizObjekt.quizArt) {
+                console.log(quizObject);
+                document.getElementById("quizContainer").innerHTML = quizObject.frage;
+                switch (quizObject.quizArt) {
                     case "multipleChoice":
                         loadHtmlIntoContainer("MultipleChoice/quizFront.html", containerId);
                         break;
@@ -27,58 +47,45 @@
                     default:
                         document.getElementById(containerId).innerHTML = "Es gab einen Fehler beim Laden des Quiz: Ungültiger Quiz-Typ:", quizObjekt.type;
                 }
-            });
-        }
-
-        async function fetchJsonObjects(){
-            try {
-                const response = await fetch("./quizJson.json");
-                const quizDaten = await response.json();
-                return quizDaten; 
-            } catch (error) {
-                console.error("Error loading quiz data:", error);
-                return []; 
             }
-        }
 
-        async function loadHtmlIntoContainer(filePath, containerId) {
-            try {
-                const response = await fetch(filePath); 
-                if (!response.ok) {
-                    throw new Error(`Failed to load HTML file: ${response.statusText}`);
+            async function loadHtmlIntoContainer(filePath, containerId) {
+                try {
+                    const response = await fetch(filePath); 
+                    if (!response.ok) {
+                        throw new Error(`Failed to load HTML file: ${response.statusText}`);
+                    }
+                    const htmlContent = await response.text();
+                    document.getElementById(containerId).innerHTML = htmlContent; 
+                } catch (error) {
+                    document.getElementById(containerId).innerHTML = "Es gab einen Fehler beim Laden des Quiz: " + error;
                 }
-                const htmlContent = await response.text(); 
-                document.getElementById(containerId).innerHTML = htmlContent; 
-            } catch (error) {
-                document.getElementById(containerId).innerHTML = "Es gab einen Fehler beim Laden des Quiz: " + error;
-            }
-        }
-
-        function loadQuizScript(quizArt){
-
-            const script = document.createElement("script");
-
-            switch(quizArt){
-                case "multipleChoice":
-                    script.src = "MultipleChoice/quizBackground.js"; 
-                    break;
-                case "simple":
-                    script.src = "Simple/quizBackground.js"; 
-                    break;
-                case "slider":
-                    script.src = "Slider/quizBackground.js"; 
-                    break;
-                default:
-                    console.error("Error loading quiz script: Invalid quiz type:", quizArt);
-                    return;
             }
 
-            document.getElementById("scriptContainer").appendChild(script);
-        }
+            function loadQuizScript(quizArt){
+
+                const script = document.createElement("script");
+
+                switch(quizArt){
+                    case "multipleChoice":
+                        script.src = "MultipleChoice/quizBackground.js"; 
+                        break;
+                    case "simple":
+                        script.src = "Simple/quizBackground.js"; 
+                        break;
+                    case "slider":
+                        script.src = "Slider/quizBackground.js"; 
+                        break;
+                    default:
+                        console.error("Error loading quiz script: Invalid quiz type:", quizArt);
+                        return;
+                }
+
+                document.getElementById("scriptContainer").appendChild(script);
+            }
 
         getRandomQuizObject();
 
         }
 
-        runQuizScript();
     });
